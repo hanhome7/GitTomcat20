7b0f05e4a8e64a4e571126fb2b314c170ce99169
hanhome7 <hanhome7@gmail.com>
2014-05-24 23:32:48 +0900

    MyTest.java

---
+++ gitTomcat20/src/gitTomcat20/MyTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package gitTomcat20;
+
+/**
+ * @author han7
+ *
+ */
+public class MyTest {
+
+	/**
+	 * 
+	 */
+	public MyTest() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
